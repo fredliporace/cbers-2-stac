711f1a78843763ced76585259086af7cbb182b86
Frederico Liporace <liporace@amskepler.com>
2019-01-13 18:53:38 -0500

    Issue #12 Elasticsearch unit test, connection only'

---
+++ sam/elasticsearch/es.py
@@ -0,0 +1,47 @@
+"""es"""
+
+import os
+from elasticsearch import Elasticsearch, RequestsHttpConnection
+from aws_requests_auth.boto_utils import BotoAWSRequestsAuth
+
+def es_connect(endpoint: str, port: int,
+               use_ssl: bool = True, verify_certs: bool = True,
+               http_auth=None):
+    """
+    Connects to ES endpoint, returns Elasticsarch
+    low level client.
+
+    :param endpoint str: Elastic endpoint
+    :param port int: endpoint TCP port
+    :return: Elastic low level client
+    :rtype: class Elasticsearch
+    """
+
+    es_client = Elasticsearch(hosts=[{'host':endpoint,
+                                      'port':port}],
+                              use_ssl=use_ssl,
+                              verify_certs=verify_certs,
+                              connection_class=RequestsHttpConnection,
+                              http_auth=http_auth)
+    return es_client
+
+def create_stac_index_handler(event, context):
+    """
+    Create STAC elasticsearch index
+    """
+
+    auth = BotoAWSRequestsAuth(aws_host=os.environ['ES_ENDPOINT'],
+                               aws_region=os.environ['AWS_REGION'],
+                               aws_service='es')
+
+    #es_client = Elasticsearch(hosts=[{'host':os.environ['ES_ENDPOINT'],
+    #                                'port':int(os.environ['ES_PORT'])}],
+    #                          connection_class=RequestsHttpConnection,
+    #                          use_ssl=True,
+    #                          http_auth=auth)
+    #print(es_client.info())
+
+    es_client = es_connect(endpoint=os.environ['ES_ENDPOINT'],
+                           port=int(os.environ['ES_PORT']),
+                           http_auth=auth)
+    print(es_client.info())
---
+++ test/elasticsearch_test.py
@@ -0,0 +1,38 @@
+"""elasticsearch_test"""
+
+import time
+import unittest
+from localstack.services import infra
+from localstack.utils.aws import aws_stack
+from sam.elasticsearch.es import es_connect
+
+class ElasticsearchTest(unittest.TestCase):
+    """ElasticsearchTest"""
+
+    def setUp(self):
+        """localstack ES setup"""
+        infra.start_infra(async=True, apis=['es', 'elasticsearch'])
+
+    def tearDown(self):
+        """localstack ES teardown"""
+        infra.stop_infra()
+
+    def test_connection(self):
+        """test_connection"""
+        # Parameters may be obtained from localstack, using
+        # default parameters for now
+        #es_url = aws_stack.get_local_service_url('elasticsearch')
+        #self.assertEqual(es_url, 'http://osboxes:4578')
+        es_client = es_connect('localhost', port=4571,
+                               use_ssl=False, verify_certs=False)
+        # Wait up to 30 seconds approximately for ES service
+        # to be up
+        for _ in range(0, 30):
+            if es_client.ping():
+                break
+            else:
+                time.sleep(1)
+        self.assertTrue(es_client.ping())
+
+if __name__ == '__main__':
+    unittest.main()
