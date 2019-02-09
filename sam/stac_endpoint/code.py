61aaeee2c3e8b99a91cb881df30a5c240ba538cd
Frederico Liporace <liporace@amskepler.com>
2019-02-09 18:53:13 -0500

    /stac endpoint

--- sam/stac_endpoint/code.py
+++ sam/stac_endpoint/code.py
@@ -1,6 +1,36 @@
 """stac_endpoint"""
 
-def handler(event, context):
+import os
+import json
+import boto3
+
+S3_CLIENT = boto3.client('s3')
+
+def get_root_catalog(bucket: str):
+    """
+    Return the root catalog
+    """
+
+    catalog = S3_CLIENT.get_object(Bucket=bucket,
+                                   Key='catalog.json')['Body'].read()
+    return json.loads(catalog)
+
+def handler(event, context): # pylint: disable=unused-argument
     """
     Lambda entry point
     """
+
+    if event['path'] == '/stac':
+        document = get_root_catalog(bucket=os.environ['CBERS_STAC_BUCKET'])
+    else:
+        raise ValueError("Path unknown: {path}".format(path=event['path']))
+
+    retmsg = {
+        'statusCode': '200',
+        'body': json.dumps(document, indent=2),
+        'headers': {
+            'Content-Type': 'application/json',
+        }
+    }
+
+    return retmsg
