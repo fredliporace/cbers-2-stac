ab02524f07e7197d7bf73f7904708a5a282bafc4
Frederico Liporace <liporace@amskepler.com>
2019-05-05 11:07:06 -0400

    Change S3 bucket

--- stac_catalogs/sync_to_aws.sh
+++ stac_catalogs/sync_to_aws.sh
@@ -1,1 +1,1 @@
-aws s3 cp . s3://cbers-stac-0-6 --recursive --exclude "*" --include "*.json" --include "*.html"
+aws s3 cp . s3://cbers-stac-0-7 --recursive --exclude "*" --include "*.json" --include "*.html"
