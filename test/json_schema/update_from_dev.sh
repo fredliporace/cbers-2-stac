d94515cea91305623633f75ded89d3e7bd7fdf60
Frederico Liporace <liporace@amskepler.com>
2020-08-18 18:57:06 -0400

    Issue #35 Updating schemas

--- test/json_schema/update_from_dev.sh
+++ test/json_schema/update_from_dev.sh
@@ -1,5 +1,18 @@
-wget -O geojson.json https://raw.githubusercontent.com/radiantearth/stac-spec/dev/item-spec/json-schema/geojson.json
+# Items
+wget -O ./item-spec/json-schema/basics.json https://raw.githubusercontent.com/radiantearth/stac-spec/dev/item-spec/json-schema/basics.json
+wget -O ./item-spec/json-schema/datetime.json https://raw.githubusercontent.com/radiantearth/stac-spec/dev/item-spec/json-schema/datetime.json
+wget -O ./item-spec/json-schema/instrument.json https://raw.githubusercontent.com/radiantearth/stac-spec/dev/item-spec/json-schema/instrument.json
+wget -O ./item-spec/json-schema/item.json https://raw.githubusercontent.com/radiantearth/stac-spec/dev/item-spec/json-schema/item.json
+wget -O ./item-spec/json-schema/licensing.json https://raw.githubusercontent.com/radiantearth/stac-spec/dev/item-spec/json-schema/licensing.json
+wget -O ./item-spec/json-schema/provider.json https://raw.githubusercontent.com/radiantearth/stac-spec/dev/item-spec/json-schema/provider.json
+
+# Catalog
+wget -O ./catalog-spec/json-schema/catalog.json https://raw.githubusercontent.com/radiantearth/stac-spec/dev/catalog-spec/json-schema/catalog.json
+
+# Collection
+wget -O ./collection-spec/json-schema/collection.json https://raw.githubusercontent.com/radiantearth/stac-spec/dev/collection-spec/json-schema/collection.json
+
 # Change to use "$ref": "item.json#/definitions/core"
-wget -O item.json https://raw.githubusercontent.com/radiantearth/stac-spec/dev/item-spec/json-schema/item.json
-wget -O catalog.json https://raw.githubusercontent.com/radiantearth/stac-spec/dev/catalog-spec/json-schema/catalog.json
-wget -O collection.json https://raw.githubusercontent.com/radiantearth/stac-spec/dev/collection-spec/json-schema/collection.json
+#wget -O item.json https://raw.githubusercontent.com/radiantearth/stac-spec/dev/item-spec/json-schema/item.json
+#wget -O catalog.json https://raw.githubusercontent.com/radiantearth/stac-spec/dev/catalog-spec/json-schema/catalog.json
+#wget -O collection.json https://raw.githubusercontent.com/radiantearth/stac-spec/dev/collection-spec/json-schema/collection.json
