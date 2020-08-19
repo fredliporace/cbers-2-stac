a70d9c4c3a5755ebadced68dfb66dfa7f271a603
Frederico Liporace <liporace@amskepler.com>
2020-08-19 16:41:06 -0400

    Updating link to correct stac version, removing collections from repo (now they are generated by a test), excluding collections from initial upload to AWS since they are generated as new items are ingested
    Issue #35

--- stac_catalogs/sync_to_aws.sh
+++ stac_catalogs/sync_to_aws.sh
@@ -1,1 +1,1 @@
-aws s3 cp . s3://cbers-stac-0-7 --recursive --exclude "*" --include "*.json" --include "*.html"
+aws s3 cp . s3://cbers-stac-1-0 --recursive --exclude "*" --include "*catalog.json" --include "*.html"
