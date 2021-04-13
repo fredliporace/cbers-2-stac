3b7a0517cd18de9927fd0eddf553548fc74c57e7
Frederico Liporace <liporace@amskepler.com>
2021-04-13 18:23:53 -0300

    Issue #44; update_catalog_tree_test and utils_test to pytest

--- test/utils_test.py
+++ test/utils_test.py
@@ -1,92 +1,94 @@
 """utils_test"""
 
 import json
-import unittest
 
-from sam.process_new_scene_queue.utils import get_collection_ids, \
-    get_collection_s3_key, get_api_stac_root, \
-    static_to_api_collection, parse_api_gateway_event, \
-    build_collection_name
-
-class UtilsTest(unittest.TestCase):
-    """UtilsTest"""
-
-    def test_build_collection_name(self):
-        """test_build_collection_name"""
-
-        collection = build_collection_name(satellite='CBERS', mission='4A',
-                                           camera='WFI')
-        self.assertEqual(collection, 'CBERS4A-WFI')
-
-        collection = build_collection_name(satellite='CBERS4',
-                                           camera='AWFI')
-        self.assertEqual(collection, 'CBERS4-AWFI')
-
-    def test_collection_utils(self):
-        """test_collection_utils"""
-
-        collections = get_collection_ids()
-        self.assertEqual(len(collections), 7)
-
-        mux_prefix = get_collection_s3_key('CBERS4-MUX')
-        self.assertEqual(mux_prefix, 'CBERS4/MUX/collection.json')
-
-    def test_get_api_stac_root(self):
-        """test_get_api_stac_root"""
-
-        with open('test/api_event.json', 'r') as jfile:
-            event = json.load(jfile)
-
-        sroot = get_api_stac_root(event=event)
-        self.assertEqual(sroot['links'][0]['self'],
-                         'https://stac.amskepler.com/v07/stac')
-        self.assertEqual(sroot['links'][1]['child'],
-                         'https://stac.amskepler.com/v07/collections/CBERS4-MUX')
-        self.assertEqual(sroot['links'][4]['child'],
-                         'https://stac.amskepler.com/v07/collections/CBERS4-PAN5M')
-
-        # Check correct number on second call
-        sroot = get_api_stac_root(event=event)
-        self.assertEqual(len(sroot['links']), 8)
-
-    def test_parse_api_gateway_event(self):
-        """test_parse_api_gateway_event"""
-
-        with open('test/api_event.json', 'r') as cfile:
-            event = json.load(cfile)
-        parsed = parse_api_gateway_event(event)
-        self.assertEqual(parsed['phost'], 'https://stac.amskepler.com')
-        self.assertEqual(parsed['ppath'], 'https://stac.amskepler.com/v07/stac')
-        self.assertEqual(parsed['spath'], 'https://stac.amskepler.com/v07/stac')
-        self.assertEqual(parsed['vpath'], 'https://stac.amskepler.com/v07')
-        self.assertEqual(parsed['prefix'], 'v07')
-
-    def test_static_to_api_collection(self):
-        """test_static_to_api_collection"""
-
-        with open('test/cbers4muxcollection.json', 'r') as cfile:
-            collection = json.load(cfile)
-        with open('test/api_event.json', 'r') as cfile:
-            event = json.load(cfile)
-        #from nose.tools import set_trace; set_trace()
-        api_collection = static_to_api_collection(collection=collection,
-                                                  event=event)
-
-        self.assertEqual(len(api_collection['links']), 4)
-        for link in api_collection['links']:
-            if link['rel'] == 'self':
-                self.assertEqual(link['href'],
-                                 'https://stac.amskepler.com/v07/collections/CBERS4-MUX')
-            elif link['rel'] == 'parent':
-                self.assertEqual(link['href'],
-                                 'https://stac.amskepler.com/v07/stac')
-            elif link['rel'] == 'root':
-                self.assertEqual(link['href'],
-                                 'https://stac.amskepler.com/v07/stac')
-            elif link['rel'] == 'items':
-                self.assertEqual(link['href'],
-                                 'https://stac.amskepler.com/v07/collections/CBERS4-MUX/items')
+from sam.layers.common.utils import (
+    build_collection_name,
+    get_api_stac_root,
+    get_collection_ids,
+    get_collection_s3_key,
+    parse_api_gateway_event,
+    static_to_api_collection,
+)
 
 
-if __name__ == '__main__':
-    unittest.main()
+def test_build_collection_name():
+    """test_build_collection_name"""
+
+    collection = build_collection_name(satellite="CBERS", mission="4A", camera="WFI")
+    assert collection == "CBERS4A-WFI"
+
+    collection = build_collection_name(satellite="CBERS4", camera="AWFI")
+    assert collection == "CBERS4-AWFI"
+
+
+def test_collection_utils():
+    """test_collection_utils"""
+
+    collections = get_collection_ids()
+    assert len(collections) == 7
+
+    mux_prefix = get_collection_s3_key("CBERS4-MUX")
+    assert mux_prefix == "CBERS4/MUX/collection.json"
+
+
+def test_get_api_stac_root():
+    """test_get_api_stac_root"""
+
+    with open("test/api_event.json", "r") as jfile:
+        event = json.load(jfile)
+
+    sroot = get_api_stac_root(event=event)
+    assert sroot["links"][0]["self"] == "https://stac.amskepler.com/v07/stac"
+    assert (
+        sroot["links"][1]["child"]
+        == "https://stac.amskepler.com/v07/collections/CBERS4-MUX"
+    )
+    assert (
+        sroot["links"][4]["child"]
+        == "https://stac.amskepler.com/v07/collections/CBERS4-PAN5M"
+    )
+
+    # Check correct number on second call
+    sroot = get_api_stac_root(event=event)
+    assert len(sroot["links"]) == 8
+
+
+def test_parse_api_gateway_event():
+    """test_parse_api_gateway_event"""
+
+    with open("test/api_event.json", "r") as cfile:
+        event = json.load(cfile)
+    parsed = parse_api_gateway_event(event)
+    assert parsed["phost"] == "https://stac.amskepler.com"
+    assert parsed["ppath"] == "https://stac.amskepler.com/v07/stac"
+    assert parsed["spath"] == "https://stac.amskepler.com/v07/stac"
+    assert parsed["vpath"] == "https://stac.amskepler.com/v07"
+    assert parsed["prefix"] == "v07"
+
+
+def test_static_to_api_collection():
+    """test_static_to_api_collection"""
+
+    with open("test/cbers4muxcollection.json", "r") as cfile:
+        collection = json.load(cfile)
+    with open("test/api_event.json", "r") as cfile:
+        event = json.load(cfile)
+    # from nose.tools import set_trace; set_trace()
+    api_collection = static_to_api_collection(collection=collection, event=event)
+
+    assert len(api_collection["links"]) == 4
+    for link in api_collection["links"]:
+        if link["rel"] == "self":
+            assert (
+                link["href"] == "https://stac.amskepler.com/v07/collections/CBERS4-MUX"
+            )
+        elif link["rel"] == "parent":
+            assert link["href"] == "https://stac.amskepler.com/v07/stac"
+        elif link["rel"] == "root":
+            assert link["href"] == "https://stac.amskepler.com/v07/stac"
+        elif link["rel"] == "items":
+            assert (
+                link["href"]
+                == "https://stac.amskepler.com/v07/collections/CBERS4-MUX/items"
+            )
