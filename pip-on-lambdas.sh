02b6349dac90133530d12a608f675ea1914f3a02
Frederico Liporace <liporace@amskepler.com>
2021-06-12 19:58:51 -0300

    act, not localstack

--- pip-on-lambdas.sh
+++ pip-on-lambdas.sh
@@ -1,7 +1,7 @@
 # Install packages on lambdas directories
 
 # PATH hack to remove directory in PATH, seems a bug in
-# localstack image.
+# act image.
 # Thanks https://stackoverflow.com/a/13135333/1259982
 echo $PATH
 export PATH=$(p=$(echo $PATH | tr ":" "\n" | grep -v "\"" | tr "\n" ":"); echo ${p%:})
